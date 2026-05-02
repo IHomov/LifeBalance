901e700b5cbc1c66eddc31a990e1e578fd459eb1
Inna Khomovska <innapariy@gmail.com>
2026-05-02 22:51:16 +0200

    feat: add video and implement btn back in TaskDetailsScreen

--- README.md
+++ README.md
@@ -127,3 +127,6 @@ The app utilizes **React Navigation v6/v7**:
 - **Navigation**: @react-navigation/native, stack, bottom-tabs
 - **Icons/Styling**: Custom StyleSheet architecture
 
+
+## 💻 Video-demonstration
+video.mp4
--- src/screens/TaskDetailsScreen.js
+++ src/screens/TaskDetailsScreen.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-const TaskDetailsScreen = ({ route }) => {
+const TaskDetailsScreen = ({ route, navigation }) => {
   const { itemId } = route.params; // Отримуємо той самий ID, який ми передали
 
   return (
     <View style={styles.container}>
+        <TouchableOpacity 
+        style={styles.backBtn} 
+        onPress={() => navigation.goBack()}
+      >
+        <Text style={styles.backText}>‹ Back</Text>
+      </TouchableOpacity>
       <Text style={styles.text}>Task Details Screen</Text>
       <Text>Displaying info for ID: {itemId}</Text>
     </View>
@@ -15,6 +21,7 @@ const TaskDetailsScreen = ({ route }) => {
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
   text: { fontSize: 20, fontWeight: 'bold' },
+  paddingTop: 60,
 });
 
 export default TaskDetailsScreen;
